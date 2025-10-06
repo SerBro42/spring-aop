2db93e6b8c35395de52bc915be9a53303933cd30
Sergey Brovko <serbro7@gmail.com>
2025-10-06 17:36:40 +0200

    Dependencies autowired and called upon in runner

--- src/main/java/com/in28minutes/spring/aop/spring_aop/SpringAopApplication.java
+++ src/main/java/com/in28minutes/spring/aop/spring_aop/SpringAopApplication.java
@@ -1,8 +1,14 @@
 package com.in28minutes.spring.aop.spring_aop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.in28minutes.spring.aop.spring_aop.business.Business1;
+import com.in28minutes.spring.aop.spring_aop.business.Business2;
 
 //In Spring Core, we used to define a local variable for applicationContext in order to instantiate the beans we need. 
 //In Spring Boot we can implement CommandLineRunner to do the same thing, using its run() method. This interface is useful
@@ -10,6 +16,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //data, or executing startup logic.
 @SpringBootApplication
 public class SpringAopApplication implements CommandLineRunner{
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	Business1 business1;
+	
+	@Autowired
+	Business2 business2;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAopApplication.class, args);
@@ -17,8 +31,8 @@ public class SpringAopApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		
+		logger.info(business1.calculateSomething());
+		logger.info(business2.calculateSomething());
 	}
 
 }
