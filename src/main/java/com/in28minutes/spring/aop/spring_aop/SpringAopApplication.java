18992286b206e1298653bb3db9d97d8a03e354d5
Sergey Brovko <serbro7@gmail.com>
2025-10-06 17:12:36 +0200

    CommandLineRunner: implementation and explanation

--- src/main/java/com/in28minutes/spring/aop/spring_aop/SpringAopApplication.java
+++ src/main/java/com/in28minutes/spring/aop/spring_aop/SpringAopApplication.java
@@ -1,13 +1,24 @@
 package com.in28minutes.spring.aop.spring_aop;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//In Spring Core, we used to define a local variable for applicationContext in order to instantiate the beans we need. 
+//In Spring Boot we can implement CommandLineRunner to do the same thing, using its run() method. This interface is useful
+//for tasks that need to run only once during the application's lifecycle, such as initializing a database, loading configuration 
+//data, or executing startup logic.
 @SpringBootApplication
-public class SpringAopApplication {
+public class SpringAopApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAopApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
