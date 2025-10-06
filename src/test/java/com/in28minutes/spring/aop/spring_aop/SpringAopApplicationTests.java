8159281d86b46f02bdb81b4966b4943770f20aec
Sergey Brovko <serbro7@gmail.com>
2025-10-06 16:41:07 +0200

    Basic project setup + AOP added manually

---
+++ src/test/java/com/in28minutes/spring/aop/spring_aop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.spring.aop.spring_aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
