19b3b0b3cbcf34cd0a23e83ea28b53d51c9c8e74
Sergey Brovko <serbro7@gmail.com>
2025-10-07 11:11:28 +0200

    Execution time measurement using '@Around' annotation

---
+++ src/main/java/com/in28minutes/spring/aop/spring_aop/aspect/MethodExecutionCalculationAspect.java
@@ -0,0 +1,27 @@
+package com.in28minutes.spring.aop.spring_aop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+//AOP
+//Configuration
+@Aspect
+@Configuration
+public class MethodExecutionCalculationAspect {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	// With this advice, we want to measure the time it takes to execute the method.
+	@Around(value = "execution(* com.in28minutes.spring.aop.spring_aop.business..*.*(..))")
+	public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+		long startTime = System.currentTimeMillis();
+		joinPoint.proceed();
+		long timeTaken = System.currentTimeMillis() - startTime;
+		logger.info("Time taken by {} is {} milliseconds", joinPoint, timeTaken);
+	}
+}
