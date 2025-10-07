1a7f1f88ade8cb58f0bb9a3e9d0b077fabdf091f
Sergey Brovko <serbro7@gmail.com>
2025-10-07 13:44:31 +0200

    Other Pointcuts

--- src/main/java/com/in28minutes/spring/aop/spring_aop/aspect/CommonJoinPointConfig.java
+++ src/main/java/com/in28minutes/spring/aop/spring_aop/aspect/CommonJoinPointConfig.java
@@ -14,4 +14,22 @@ public class CommonJoinPointConfig {
 
 	}
 
+	//Pointcut that includes both the above
+	@Pointcut("com.in28minutes.spring.aop.spring_aop.aspect.CommonJoinPointConfig.businessLayerExecution() "
+			+ "&& com.in28minutes.spring.aop.spring_aop.aspect.CommonJoinPointConfig.dataLayerExecution()")
+	public void allLayerExecution() {
+		
+	}
+	
+	//Pointcut that includes any bean that includes the word 'dao'
+	@Pointcut("bean(*dao*)")
+	public void beanContainingDao() {
+		
+	}
+	
+	//Pointcut that includes any directory that includes the specified string
+	@Pointcut("within(com.in28minutes.spring.aop.spring_aop.data..*)")
+	public void dataLayerExecutionWithWithin() {
+		
+	}
 }
